f7864dd17feb7797e2a83494bd80c068ba74e158
rafabolivar <37837380+rafabolivar@users.noreply.github.com>
2021-12-09 12:13:17 +0100

    Update 6-K8s_Dashboard.sh

--- scripts/6-K8s_Dashboard.sh
+++ scripts/6-K8s_Dashboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/aio/deploy/recommended.yaml
 
 kubectl proxy
 
